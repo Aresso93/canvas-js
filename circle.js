81a75b742e498c219a4a628b3107d3ab7762e9c8
Aresso93 <ares.fiumicelli@gmail.com>
2023-07-16 15:41:58 +0200

    Opera d'arte

--- circle.js
+++ circle.js
@@ -6,24 +6,43 @@ class Circle{
         this.originY = originY;
         this.radius = radius;
         this.color = color;
-        this.velocityX = 1;
-        this.velocityY = 1;
+        this.velocityX = 100 //(2 * Math.random()) - 1;
+        this.velocityY = 100 //(2 * Math.random()) - 1;
     }
 
-    animate(){
-
-         this.velocityX = 2 * Math.random() -1;
-        this.velocityY = 2 * Math.random() -1;          
-
-        if (Math.random() > 0.98){
-            this.velocityX = (2 * Math.random()) -1;
-        } 
-
-        if (Math.random() > 0.98){
-            this.velocityY = (2 * Math.random()) -1;
+    animate(canvasWidth, canvasHeight) {
+        if (Math.random() > 0.99) {
+            this.velocityX = (2 * Math.random()) - 1;
+        }
+    
+        if (Math.random() > 0.99) {
+            this.velocityY = (2 * Math.random()) - 1;
+        }
+    
+        const nextX = this.originX + this.velocityX;
+        const nextY = this.originY + this.velocityY;
+    
+        if (nextX + this.radius >= canvasWidth) {
+            this.originX = canvasWidth - this.radius;
+            this.velocityX = -Math.abs(this.velocityX);
+        } else if (nextX - this.radius <= 0) {
+            this.originX = this.radius;
+            this.velocityX = Math.abs(this.velocityX);
+        } else {
+            this.originX += this.velocityX;
+        }
+    
+        if (nextY + this.radius >= canvasHeight) {
+            this.originY = canvasHeight - this.radius;
+            this.velocityY = -Math.abs(this.velocityY);
+        } else if (nextY - this.radius <= 0) {
+            this.originY = this.radius;
+            this.velocityY = Math.abs(this.velocityY);
+        } else {
+            this.originY += this.velocityY;
         }
     }
-
+    
 
     draw(ctx){
         ctx.beginPath()
@@ -33,7 +52,25 @@ class Circle{
     }
 
 
+    static random(canvasWidth, canvasHeight){
 
+        const randomRed = Math.floor(256 * Math.random());
+        const randomGreen = Math.floor(256 * Math.random());
+        const randomBlue = Math.floor(256 * Math.random());
+
+        const rgbString = 'rgba(' + randomRed + ', ' + randomGreen + ', ' + randomBlue + ', ' + Math.random() + ')'
+
+        const randomWidth = 200 * Math.random();
+        const randomHeight = 200 * Math.random();
+
+        const randomX = (canvasWidth  - randomWidth) * Math.random();
+        const randomy = (canvasHeight - randomHeight) * Math.random();
+
+        const newCircle = new Circle(canvasWidth/2, canvasHeight/2, 50, rgbString);
+
+        return newCircle;
+
+    }
 
 
 
--- main-circle.js
+++ main-circle.js
@@ -2,13 +2,36 @@ const canvas = document.getElementById("canvas1");
 
 const ctx = canvas.getContext("2d");
 
-const c1 = new Circle(canvas.width/2, canvas.height/2, 200);
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
 
-c1.draw(ctx);
+const c1 = new Circle(canvasWidth/2, canvasHeight/2, 80);
 
-function moveCircle(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    c1.animate();
-    c1.draw(ctx);
+
+// function moveCircle(){
+//     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+//     c1.animate(canvasWidth, canvasHeight);
+//     c1.draw(ctx);
+//     requestAnimationFrame(moveCircle);
+// }
+
+
+
+const cirlceArray = [];
+
+for (let i = 0; i < 6000; i++) {
+    const circle = Circle.random(canvas.width, canvas.height);
+    cirlceArray.push(circle);
 }
+
+//moveCircle();
     
+function moveAll(){
+    for (const circle of cirlceArray) {
+        circle.animate(canvasWidth, canvasHeight);
+        circle.draw(ctx);
+    }
+    requestAnimationFrame(moveAll);
+}
+
+requestAnimationFrame(moveAll);
